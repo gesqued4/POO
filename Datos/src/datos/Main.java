c7fe122a45c8e519043401a5a66431328950b750
Gerardo Esqueda Padilla <150204906+gesqued4@users.noreply.github.com>
2024-10-24 15:47:36 -0600

    Add files via upload
    Datos

---
+++ Datos/src/datos/Main.java
@@ -0,0 +1,33 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datos;
+/*
+Bibliotecas de funciones
+*/
+import java.util.Scanner;
+/**
+ *
+ * @author pades
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Instanciar objeto Scanner
+        Scanner sc = new Scanner(System.in);
+        // Variables
+        int edad;
+        edad = sc.nextInt();
+        
+    }//Fin de funcion main
+    
+    public static void print(String txt)
+    
+}
